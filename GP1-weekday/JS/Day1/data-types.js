0fcb9b9be3dc0360219be516f7c8f2a837d8f569
mtcodingtech <mtcodingtech@gmail.com>
2025-01-01 15:48:54 +0630

    js

---
+++ GP1-weekday/JS/Day1/data-types.js
@@ -0,0 +1,42 @@
+
+// Number
+// let age = 20.3;
+// console.log(age, typeof age)
+
+//String
+// let message = "Hello";
+// let username = "John";
+// // let result = message + " " + username + ", " + "How are you?";
+// let result = `${message} ${username}, How are you?`; // string template or template literal 
+// console.log(result)
+
+// Boolean
+// let isGreater = 10 > 4;
+// console.log(isGreater)
+
+// let username;
+// console.log(username, typeof username);
+
+// let age = null;
+// console.log(age, typeof age);
+
+// console.log(username == age) // value
+// console.log(username === age) // value and data type
+
+// let fruits = ["apple", "orange", "mango"];
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[3]) // underfined
+
+// let user = {
+//     name: "John",
+//     age: 20
+// }
+// // console.log(user.name, user.age)
+// console.log(user["name"], user["age"])
+
+
+// alert("Hello World")
+// confirm("How are you?")
+prompt("Are you a boss?")
---
+++ GP1-weekday/JS/Day1/script.js
@@ -0,0 +1,33 @@
+"use strict";
+// var username = "John";
+// username = "Rose";
+
+// let age = 20;
+// age = 30;
+
+// const city = "Ygn";
+// city = "Mdy";
+
+// console.log("username", username);
+// console.log("age", age);
+// console.log("city", city)
+
+// let message = "Hello";
+// let greet = message;
+
+// console.log(greet)
+
+// var username = "John";
+
+// if(true){
+//     var username = "Rose";
+//     console.log(username)
+// }
+
+// console.log(username)
+
+num = 5;
+let num = 5;
+console.log(num)
+
+
