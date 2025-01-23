2abd6af6aa23b1abd58f008cb1b100da19fcb9e8
mtcodingtech <mtcodingtech@gmail.com>
2025-01-23 10:32:42 +0630

    remove book

--- React-batch-2/redux-basic/src/components/BooksList.js
+++ React-batch-2/redux-basic/src/components/BooksList.js
@@ -1,39 +1,52 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { addBook } from '../redux/booksSlice';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addBook, removeBook } from "../redux/booksSlice";
 
 function BooksList() {
-    const [name, setName] = useState("");
-    const [author, setAuthor] = useState("");
-    const books = useSelector(state => state.books);
-    console.log(books)
-    const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const books = useSelector((state) => state.books);
+  console.log(books);
+  const dispatch = useDispatch();
 
-    const handleAdd = () => {
-        if(name && author){
-            dispatch(addBook({name, author}))
-            setName("")
-            setAuthor("")
-        }
+  const handleAdd = () => {
+    if (name && author) {
+      dispatch(addBook({ name, author }));
+      setName("");
+      setAuthor("");
     }
+  };
+
   return (
-   <>
-    <h1>Books</h1>
-    <input type="text" value={name} onChange={(e) => setName(e.target.value)} required placeholder='Please add a book title' />
-    <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} required placeholder='Please add author name' />
-    <button onClick={handleAdd}>Add Book</button><br />
-    {
-        books.map((book, index) => {
-            return(
-                <React.Fragment key={index}>
-                   <strong>{book.name}</strong>
-                   <p>{book.author}</p>
-                </React.Fragment>
-            )
-        })
-    }
-   </>
-  )
+    <>
+      <h1>Books</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+        placeholder="Please add a book title"
+      />
+      <input
+        type="text"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        required
+        placeholder="Please add author name"
+      />
+      <button onClick={handleAdd}>Add Book</button>
+      <br />
+      {books.map((book, index) => {
+        return (
+          <React.Fragment key={index}>
+            <strong>{book.name}</strong>
+            <p>{book.author}</p>
+            <button onClick={() => dispatch(removeBook(index))}>Remove</button>
+          </React.Fragment>
+        );
+      })}
+    </>
+  );
 }
 
-export default BooksList
+export default BooksList;
--- React-batch-2/redux-basic/src/redux/booksSlice.js
+++ React-batch-2/redux-basic/src/redux/booksSlice.js
@@ -1,16 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const booksSlice = createSlice({
-    name: "books",
-    initialState: [],
-    reducers: {
-       addBook: (state, action) => {
-        console.log(action)
-        state.push(action.payload)
-       },
-     
-    }
-})
+  name: "books",
+  initialState: [],
+  reducers: {
+    addBook: (state, action) => {
+      state.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      return state.filter((_, index) => index !== action.payload);
+    },
+  },
+});
 
-export const { addBook } = booksSlice.actions;
+export const { addBook, removeBook } = booksSlice.actions;
 export default booksSlice.reducer;
